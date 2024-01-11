caf005b37a4e86494581740250f97c95ff68e418
Ezequiel <ezealba2@gmail.com>
2024-01-11 12:08:43 -0300

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int guess;
+        int count = 0;
+        
+       do{
+            Console.WriteLine("What is your guess?: ");
+            string guess_str = Console.ReadLine();
+            guess = int.Parse(guess_str);
+            count++;
+    
+            if (guess == number)
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You made {count} guesses!");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        while(guess != number);
+
+        }
 }
