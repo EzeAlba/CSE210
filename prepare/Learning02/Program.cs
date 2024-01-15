6c9e9ff747ab6316ac542f579765a6f3b4084b96
Ezequiel <ezealba2@gmail.com>
2024-01-15 19:09:06 -0300

    learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        
+        Job job2 = new Job();
+
+        job2._jobTitle = "IT Support";
+        job2._company = "Google";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Ezequiel Albarracin";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        
+        resume1.Display();
     }
 }
